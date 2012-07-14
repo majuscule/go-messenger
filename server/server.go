636d1563acde603149036f24a62954096ceba0f2
Robert Picard <mail@robert.io>
2012-07-13 23:12:57 -0400

    Adding dispatch function

--- server/server.go
+++ server/server.go
@@ -23,6 +23,7 @@ func main() {
 
     for {
         conn, err := ln.Accept()
+
         if err != nil {
             fmt.Println("[ERROR] Connection failed.")
 
@@ -35,11 +36,21 @@ func main() {
 
 func handleConnection(conn net.Conn) {
     fmt.Println("[SUCCESS] Connection received.")
+
+    // message  := conn.Read()
+
     fmt.Println(io.Copy(os.Stdout, conn))
 
-    go dispatchMessage(message, target)
+    go dispatchMessage("message")
 }
 
-func dispatchMessage(message string, target string) {
+func dispatchMessage(message string) {
+    fmt.Println("Message dispatch called.")
+    conn, err := net.Dial("tcp", "localhost:8091")
 
+    if err != nil {
+        fmt.Println("[ERROR] Failed to connect to client")
+    }
+
+    fmt.Fprintf(conn, message)
 }
