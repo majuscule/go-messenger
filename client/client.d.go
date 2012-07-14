276ae9119c974081bc251bf8cedfdc12d54575ac
Robert Picard <mail@robert.io>
2012-07-13 23:13:24 -0400

    Add the client-side listener

---
+++ client/client.d.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "net"
+    "io"
+    "os"
+)
+
+const (
+    OUR_PORT = "8091"
+)
+
+func main() {
+
+    ln, err := net.Listen("tcp", ":" + OUR_PORT)
+
+    if err != nil {
+        fmt.Println("[ERROR] Failed to listen on port " + OUR_PORT + ".")
+    }
+
+    fmt.Println("Listening on port " + OUR_PORT + "...")
+
+    for {
+        conn, err := ln.Accept()
+        if err != nil {
+            fmt.Println("[ERROR] Connection failed.")
+
+            continue
+        }
+
+        go handleConnection(conn)
+    }
+}
+
+func handleConnection(conn net.Conn) {
+    fmt.Println("[SUCCESS] Connection received.")
+    fmt.Println(io.Copy(os.Stdout, conn))
+}
