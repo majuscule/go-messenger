fd584a3b2b647cf3d3dcf7c821beff25747abc88
Robert Picard <mail@robert.io>
2012-07-13 23:32:41 -0400

    Now you don't have to close the client manually.

--- client/client.go
+++ client/client.go
@@ -3,7 +3,6 @@ package main
 import (
     "fmt"
     "net"
-    "bufio"
     "flag"
 )
 
@@ -12,14 +11,17 @@ const (
     SERVER_PORT = "8090"
 )
 
+
 func main() {
     var message string
     parseArguments(&message)
+
     conn, _ := net.Dial("tcp", SERVER_ADDR + ":" + SERVER_PORT)
     fmt.Fprintf(conn, message)
-    status, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Println(status)
+
+    return
 }
+
 
 func parseArguments(message *string) {
     flag.StringVar(message, "message", "", "tell a ducktern")
